68501c7c279a65c04948b23ab2bcebb8f84d8357
hughhhh <hmiles@lyft.com>
2019-01-06 20:16:40 -0800

    working cli taking proto package and generates endpoint

--- hellowmars/flask-blueprint/{{cookiecutter.app_dir}}/app/routes/{{cookiecutter.blueprint_name}}.py
+++ hellowmars/flask-blueprint/{{cookiecutter.app_dir}}/app/routes/{{cookiecutter.blueprint_name}}.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, jsonify, request
 
-blueprint = Blueprint("{{cookiecutter.blueprint_name}}", __name__)
-
+{% for service in cookiecutter.services.buffer %}
+blueprint = Blueprint("{{service.service_name}}", __name__)
 
 @blueprint.route("/api/hello", methods=["GET"])
-def hello_world():
+def {{service.message_name}}():
     # your data transformation logic should go in here.
     # Business logic should be made into new modules and called from the endpoint.
     name = "Hugh"
+    # package_name.{{service.output}}()
     return jsonify({"msg": "Hello there, {}!".format(name or "world")})
+
+{% endfor %}
